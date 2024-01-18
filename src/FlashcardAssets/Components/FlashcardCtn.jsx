c20999a7b9845d73ff1ffdb9c8db4baa090df54a
joshjones49 <joshjones200285@gmail.com>
2024-01-18 17:00:53 -0600

    css for flashcards set, working on slipping between question and answer

--- src/FlashcardAssets/Components/FlashcardCtn.jsx
+++ src/FlashcardAssets/Components/FlashcardCtn.jsx
@@ -1,36 +1,25 @@
 import { useContext } from "react";
 import FlashcardContext from "../ContextProvider/FlashcardContext.mjs";
 import './flashcards.css';
-import { TiPin } from "react-icons/ti";
-import { BiSolidLeftArrowSquare } from "react-icons/bi";
-import { BiSolidRightArrowSquare } from "react-icons/bi";
 import { BsSearch } from "react-icons/bs";
+import IndFlashcard from "./IndFlashcard";
 
 const FlashcardCtn = () => {
 
+    const {flashcards} = useContext(FlashcardContext)
+
     return (
         <div className="FlashcardCtn" >
-            <div className="SearchBar" >
-                <input placeholder="Search Flashcards"/>
-                <BsSearch className="BsSearch" />
-            </div>
-            <div className="Flashcard" >
-                <h1>Question</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas consectetur illo doloremque eligendi in eveniet dolorem architecto ipsum quis, facere libero ratione? Optio quia totam sint adipisci error, dolorem repudiandae!</p>
-                <div>
-                    <div className="FlashcardBtnDiv" >
-                        <BiSolidLeftArrowSquare className="Arrow" />
-                        <TiPin className="Pin" />
-                    </div>
-
-                    <div className="FlashcardBtnDiv" >
-                        <button className="AnswerBtn" >Answer</button>
-                        <BiSolidRightArrowSquare className="Arrow" />
-                    </div>
-                </div>
-            </div>
+            {flashcards.map((flashcard) => (
+                <IndFlashcard key={flashcard.id} flashcard={flashcard} />
+            ))}
         </div>
   )
 }
 
 export default FlashcardCtn;
+
+{/* <div className="SearchBar" >
+                <input placeholder="Search Flashcards"/>
+                <BsSearch className="BsSearch" />
+            </div> */}
