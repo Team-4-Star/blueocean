44d4ac5bc4b4df9c121dc1e92503b256d91efb1e
nateykliu <nathanykliu@gmail.com>
2024-01-19 11:31:29 -0800

    home component

---
+++ components/Home.jsx
@@ -0,0 +1,15 @@
+
+import Navbar from '../components/Navbar'
+
+function Home() {
+
+  return (
+    <div className="main-page">
+      <div className="page-content">
+        <h1>These are the instructions.</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
