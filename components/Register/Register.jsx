0c736ce11436624677aef1984c81b17609f99f4f
Drew Kullman <agkullman@protonmail.com>
2024-01-28 13:24:54 -0900

    Added initial registration form

---
+++ components/Register/Register.jsx
@@ -0,0 +1,29 @@
+import './Register.css'
+
+const Register = () => {
+
+    return (
+        <>
+        <div className="register-container">
+            <h1>Registration Form</h1>
+            <div className = "input-container">
+                <div className ="input-row">
+                    <label for="username" class="label">Username:</label>
+                    <input type="text" id="username" placeholder="username" autocomplete="off"/> 
+                </div>
+                <div className ="input-row">
+                    <label for="password" class="label">Password</label>
+                    <input type="text" id="password" placeholder="password" autocomplete="off"/>
+                </div>
+                <button id="submit-btn">Submit</button>
+                <div id="status" className="hidden">
+                    PLACEHOLDER
+                </div>
+            </div>
+        </div>           
+        </>
+    );
+
+}
+
+export default Register;
