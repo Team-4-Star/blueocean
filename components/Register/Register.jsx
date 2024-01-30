37e842c422b2359f94490353674d68df4b7e7470
Drew Kullman <agkullman@protonmail.com>
2024-01-30 08:27:31 -0900

    Added Login

--- components/Register/Register.jsx
+++ components/Register/Register.jsx
@@ -1,33 +1,56 @@
-import './Register.css'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Register.css';
 
 const Register = () => {
+   
+    const [registrationStatus, setRegistrationStatus] = useState({
+        message: 'Registration Successful',
+        visible: false
+    });
+
+    // Access the history object from React Router
+    const navigate = useNavigate();
+
+    // Function to handle form submission
+    const handleSubmit = () => {
+        // Simulated registration logic
+        // After successful registration, show the status message for 2 seconds
+        setRegistrationStatus({ message: 'Registration Successful', visible: true });
+        setTimeout(() => {
+            setRegistrationStatus({ ...registrationStatus, visible: false });
+            // Redirect to the home page after successful registration
+            navigate('/');
+        }, 2000);
+    };
 
     return (
         <>
-        <div className="register-container">
-            <h1>Registration Form</h1>
-            <div className = "input-container">
-                <div className ="input-row">
-                    <label for="username" class="label">Username:</label>
-                    <input type="text" id="username" placeholder="username" autocomplete="off"/> 
-                </div>
-                <div className ="input-row">
-                    <label for="password" class="label">Password:</label>
-                    <input type="text" id="password" placeholder="password" autocomplete="off"/>
-                </div>
-                <div className ="input-row">
-                    <label for="role" class="label">Role:</label>
-                    <input type="text" id="role" placeholder="role" autocomplete="off"/>
-                </div>
-                <button id="submit-btn">Submit</button>
-                <div id="status" className="hidden">
-                    PLACEHOLDER
+            <div className="register-container">
+                <div className="register-border">
+                    <h1 className="header-label">REGISTER ACCOUNT</h1>
+                    <div className="input-container">
+                        <div className="input-row">
+                            <label htmlFor="username" className="label">USERNAME</label>
+                            <input type="text" className="input-value" id="username" placeholder="username" autoComplete="off"/>
+                        </div>
+                        <div className="input-row">
+                            <label htmlFor="password" className="label">PASSWORD</label>
+                            <input type="password" className="input-value" id="password" placeholder="password" autoComplete="off"/>
+                        </div>
+                        <div className="input-row">
+                            <label htmlFor="role" className="label">ROLE</label>
+                            <input type="text" className="input-value" id="role" placeholder="role" autoComplete="off"/>
+                        </div>
+                    </div>
+                    <button id="submit-btn" onClick={handleSubmit}>SUBMIT</button>
+                    <div id="status" className={registrationStatus.visible ? '' : 'hidden'}>
+                            {registrationStatus.message}
+                    </div>
                 </div>
             </div>
-        </div>           
         </>
     );
-
-}
+};
 
 export default Register;
--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Home from '../components/Home.jsx'
 import FlashcardCtn from '../components/Flashcards/Components/FlashcardCtn.jsx';
 import CategoriesCtn from '../components/Flashcards/Components/CategoriesCtn.jsx';
 import Register from '../components/Register/Register.jsx'
+import Login from '../components/Login/Login.jsx'
 import ReactCardCtn from '../components/Flashcards/Components/ReactCardCtn.jsx';
 import NodeCardCtn from '../components/Flashcards/Components/NodeCardCtn.jsx';
 
@@ -23,6 +24,7 @@ function App() {
           <Route path = "/categories" element={<CategoriesCtn />} />
           <Route path = "/flashcards" element={<FlashcardCtn />} />
           <Route path = "/register" element={<Register/>} />
+          <Route path = "/login" element={<Login/>} />
           <Route path = "/react-flashcards" element={<ReactCardCtn/>} />
           <Route path = "/node-flashcards" element={<NodeCardCtn/>} />
         </Routes>
