c577ae79ae6393c9ddcb89fb6f9205de7c6331ea
Drew Kullman <agkullman@protonmail.com>
2024-01-31 10:34:15 -0900

    Restyled Home Component

--- components/Home.jsx
+++ components/Home.jsx
@@ -6,8 +6,24 @@ function Home() {
 
   return (
       <div className="home-page-content">
-        <h1>Welcome to the app! To review your flashcards, click the button at the top to continue to the "Flashcards" section, or the "Commands" section for a list of commonly used psql commands.
-        </h1>
+        <h1 className="home-page-header">Welcome</h1>
+        <ul className="home-page-text">
+
+          <h2 className="home-page-section-header"> Flashcards </h2>
+            <li>
+            Review React and Node flashcards
+            </li>
+            <li>
+            Create new flashcards
+            </li>
+          <h2 className="home-page-section-header"> Commands  </h2>
+            <li>
+            Search through commands
+            </li>
+            <li>
+            Click copy icon to capture command snippet for project
+            </li>  
+        </ul>
       </div>
   )
 }
